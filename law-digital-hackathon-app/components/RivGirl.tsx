8b28f9be21379ddce16290435495ec2b1c3e2ef3
v_goh <vanngoh.dev@gmail.com>
2023-11-16 01:47:38 +0900

    touch up ui

--- law-digital-hackathon-app/components/RivGirl.tsx
+++ law-digital-hackathon-app/components/RivGirl.tsx
@@ -1,15 +1,15 @@
-"use client"
-import { useRive } from "@rive-app/react-canvas";
+'use client';
+import { useRive } from '@rive-app/react-canvas';
 
 export const RivGirl = () => {
-    const { RiveComponent } = useRive({
-        src: "/girl.riv",
-        autoplay: true,
-    });
+  const { RiveComponent } = useRive({
+    src: '/girl.riv',
+    autoplay: true,
+  });
 
-    return (
-        <div className="max-h-screen ps-64" style={{ width: "700px", height: "900px" }}>
-            <RiveComponent />
-        </div>
-    )
-}
+  return (
+    <div className="min-h-screen" style={{ width: '500px', height: '100vh' }}>
+      <RiveComponent />
+    </div>
+  );
+};
