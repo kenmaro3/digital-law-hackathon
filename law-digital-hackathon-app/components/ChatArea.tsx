99ad7bcbeb445524df83b4c53292e17abb97a491
kenmaro3 <miha.ken.19@gmail.com>
2023-11-16 03:31:08 +0900

    [REF] submit button handle

--- law-digital-hackathon-app/components/ChatArea.tsx
+++ law-digital-hackathon-app/components/ChatArea.tsx
@@ -59,7 +59,14 @@ export const ChatArea = () => {
 
   // 6. Function to handle all submits
   const handleAllSubmits = (e: any) => {
+    e.preventDefault();
     if (e.key === 'Enter') {
+      if (composing) return;
+      handleSubmit(e as FormEvent<HTMLFormElement>, {});
+      setInput('');
+      setIsLoading(true);
+    }
+    if(e.type == "submit"){
       if (composing) return;
       handleSubmit(e as FormEvent<HTMLFormElement>, {});
       setInput('');
@@ -118,25 +125,23 @@ export const ChatArea = () => {
             className="h-full mx-3 flex flex-col overflow-y-auto overflow-x-hidden">
             {messages.length > 0
               ? messages.map((m) => (
-                  <div
-                    key={m.id}
-                    className={`${
-                      m.role === 'user'
-                        ? 'flex justify-end'
-                        : 'flex justify-start'
+                <div
+                  key={m.id}
+                  className={`${m.role === 'user'
+                    ? 'flex justify-end'
+                    : 'flex justify-start'
                     } my-1`}>
-                    <div
-                      className={`max-w-[60%] px-4 py-2 rounded-lg ${
-                        m.role === 'user'
-                          ? 'bg-gradient-to-r from-violet-600 to-fuchsia-500 text-white'
-                          : 'bg-gray-200 opacity-80'
+                  <div
+                    className={`max-w-[60%] px-4 py-2 rounded-lg ${m.role === 'user'
+                      ? 'bg-gradient-to-r from-violet-600 to-fuchsia-500 text-white'
+                      : 'bg-gray-200 opacity-80'
                       }`}>
-                      <ReactMarkdown components={{ a: LinkRenderer }}>
-                        {m.content}
-                      </ReactMarkdown>
-                    </div>
+                    <ReactMarkdown components={{ a: LinkRenderer }}>
+                      {m.content}
+                    </ReactMarkdown>
                   </div>
-                ))
+                </div>
+              ))
               : null}
 
             {isLoading && (
